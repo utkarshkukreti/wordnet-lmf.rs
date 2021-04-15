fe6de1c89c39a7ebc4824b21f2b510bf3f6ed437
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2021-04-15 19:02:15 +0530

    add an example which reads lmf file from arg and prints some stats
    The library is taking 2.6 seconds to read english-wordnet-2020.xml and
    using 181MB max resident memory, as measured using gnu-time.

---
+++ examples/read.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let path = std::env::args()
+        .nth(1)
+        .expect("must pass lmf file path as argument");
+    let timer = std::time::Instant::now();
+    let file = wordnet_lmf::File::open(path).unwrap();
+    dbg!(timer.elapsed());
+    dbg!(file.lexicons.len());
+    for lexicon in file.lexicons {
+        dbg!(
+            lexicon.id,
+            lexicon.label,
+            lexicon.language,
+            lexicon.lexical_entries.len(),
+            lexicon.synsets.len()
+        );
+    }
+}
